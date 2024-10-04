3515863baaadd2a94e6766670b7d354d70ec4d1f
ndoppler <112019993+ndoppler@users.noreply.github.com>
2024-10-03 20:00:15 -0500

    modal creation

--- client/src/components/CharacterModal.jsx
+++ client/src/components/CharacterModal.jsx
@@ -1,19 +1,20 @@
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import CharacterForm from './CharacterForm';
 
 const CharacterModal = ({ isOpen, toggle }) => {
+  const handleFormSubmit = (formData) => {
+    toggle();
+  };
   return (
     <div>
       <Modal isOpen={isOpen} toggle={toggle}>
         <ModalHeader>Add a Character</ModalHeader>
         <ModalBody>
-          Add character Here
+          <CharacterForm onSubmit={handleFormSubmit} />
         </ModalBody>
         <ModalFooter>
           <Button color="secondary" onClick={toggle}>
             Close
-          </Button>
-          <Button color="primary" onClick={() => alert('Action')}>
-            Submit
           </Button>
         </ModalFooter>
       </Modal>
